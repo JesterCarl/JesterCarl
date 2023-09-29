f34fc9fae1718593495841a46cd10040b5a95049
JesterCarl <146396074+JesterCarl@users.noreply.github.com>
2023-09-29 08:26:52 +0800

    Add files via upload

---
+++ Sample 6 jc.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+double a, b, total;	
+	
+	
+	
+	cout<<"Enter salary per day: ";
+	cin>>a;// Input Salary
+	
+	cout<<"Enter number od Days Work: ";
+	cin>>b;// Input Days of Work
+	
+	total= a * b;
+	
+	cout<<"Your total Salary is:"<<total;
+	
+	/* Jester Carl T. Cahilig
+	 BSCS-1-A
+	*/
+	
+	return 0;
+}
